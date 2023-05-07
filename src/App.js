fe8004c1468fee4e9f58996f05b6eb16d7ec6d08
nass <cabliveer@gmail.com>
2023-05-07 07:31:19 +0200

    save

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import Home from "./pages/home/Home";
+
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import PrivateRoute from "./components/privateRoute/PrivateRoute";
+ 
+
+
+function App() {
+
+  
+
+  
+  return (
+     
+
+    <div className="app">
+      
+        <BrowserRouter>
+
+          <Routes>
+            <Route path="/">
+
+              <Route index element={
+
+                 
+                  <Home />
+                 
+              } />
+
+              
+                
+               
+                
+                
+                
+               
+                
+
+              
+              </Route>
+              
+           
+          </Routes>
+
+        </BrowserRouter>
+       
+    </div>
+
+   
+
+  );
+}
+
+export default App;
